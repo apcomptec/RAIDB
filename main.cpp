0df1b7b5818d7fd5998e170f8f6d34be2b51de35
Jose Daniel <josebaduma@gmail.com>
2013-05-03 20:06:18 -0600

    Archivos

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include<stdlib.h>
+#include<time.h>
+#include <fstream>
 
 using namespace std;
 
+
+char* getRandomString();
+
 int main()
 {
-    cout << "Hello World!" << endl;
+
+    ofstream binaryFile ("file.raw", ios::out | ios::binary);
+    char* myData;
+    for (int var = 0; var < 1024; ++var) {
+        myData = getRandomString();
+        cout << myData << endl;
+        binaryFile.write (myData, sizeof (char));
+        binaryFile.write ("\n", sizeof (char));
+    }
+    binaryFile.close();
     return 0;
+}
+
+char* getRandomString(){
+    srand(time(NULL));
+    char strrnd[20];
+    for(int i=0; i < 20; i++){
+        strrnd[i] = 65 + (rand() % (90-65));
+    }
+    return strrnd;
 }
