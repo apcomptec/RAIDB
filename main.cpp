d1b7bd8361906f04dff3f3c90ce610d42481edab
brallan <apcomptec@gmail.com>
2013-05-03 20:20:35 -0600

    - Crear archivos de tamaño definido

--- main.cpp
+++ main.cpp
@@ -1,33 +1,64 @@
-#include <iostream>
-#include<stdlib.h>
-#include<time.h>
+//#include <iostream>
+//#include<stdlib.h>
+//#include<time.h>
+//#include <fstream>
+//#include <QString>
+//#include <QCryptographicHash>
+
+//using namespace std;
+
+
+//char* getRandomString();
+
+//int main()
+//{
+
+//    ofstream binaryFile("file.raw", ios::out | ios::binary);
+//    char* myData;
+//    for (int var = 0; var < 1024; ++var) {
+//        myData = getRandomString();
+//        cout << myData << endl;
+//        binaryFile.write(myData, sizeof(char));
+//        binaryFile.write("\n", sizeof(char));
+//    }
+//    binaryFile.close();
+//    return 0;
+
+////    QString blah = QString(QCryptographicHash::hash(("jejeje"),
+////                           QCryptographicHash::Md5).toHex());
+
+////    std::cout << blah.toStdString();
+
+//}
+
+//char* getRandomString()
+//{
+//    srand(time(NULL));
+//    char strrnd[20];
+//    for (int i = 0; i < 20; i++) {
+//        strrnd[i] = 65 + (rand() % (90 - 65));
+//    }
+//    return strrnd;
+//}
+
+///* fwrite example : write buffer */
+//#include <stdio.h>
+
+//int main ()
+//{
+//    FILE * pFile;
+//    long long buffer[] = { 'x' , 'y' , 'z' };
+//    pFile = fopen ( "myfile.txt" , "wb" );
+//    fwrite (buffer , 130000, sizeof(buffer) , pFile );
+//    fclose (pFile);
+//    return 0;
+//}
+
 #include <fstream>
-
-using namespace std;
-
-
-char* getRandomString();
 
 int main()
 {
-
-    ofstream binaryFile ("file.raw", ios::out | ios::binary);
-    char* myData;
-    for (int var = 0; var < 1024; ++var) {
-        myData = getRandomString();
-        cout << myData << endl;
-        binaryFile.write (myData, sizeof (char));
-        binaryFile.write ("\n", sizeof (char));
-    }
-    binaryFile.close();
-    return 0;
-}
-
-char* getRandomString(){
-    srand(time(NULL));
-    char strrnd[20];
-    for(int i=0; i < 20; i++){
-        strrnd[i] = 65 + (rand() % (90-65));
-    }
-    return strrnd;
+    std::ofstream ofs("ouput.img", std::ios::binary | std::ios::out);
+    ofs.seekp((1024<<20) - 1);
+    ofs.write("", 1);
 }
