ab9dbc73c96f980e4970d98b443af4c7fd67797d
Jose Daniel <josebaduma@gmail.com>
2013-05-05 00:04:05 -0600

    Lectura y escritura de archivos

--- main.cpp
+++ main.cpp
@@ -1,64 +1,42 @@
-//#include <iostream>
-//#include<stdlib.h>
-//#include<time.h>
-//#include <fstream>
-//#include <QString>
-//#include <QCryptographicHash>
-
-//using namespace std;
-
-
-//char* getRandomString();
-
-//int main()
-//{
-
-//    ofstream binaryFile("file.raw", ios::out | ios::binary);
-//    char* myData;
-//    for (int var = 0; var < 1024; ++var) {
-//        myData = getRandomString();
-//        cout << myData << endl;
-//        binaryFile.write(myData, sizeof(char));
-//        binaryFile.write("\n", sizeof(char));
-//    }
-//    binaryFile.close();
-//    return 0;
-
-////    QString blah = QString(QCryptographicHash::hash(("jejeje"),
-////                           QCryptographicHash::Md5).toHex());
-
-////    std::cout << blah.toStdString();
-
-//}
-
-//char* getRandomString()
-//{
-//    srand(time(NULL));
-//    char strrnd[20];
-//    for (int i = 0; i < 20; i++) {
-//        strrnd[i] = 65 + (rand() % (90 - 65));
-//    }
-//    return strrnd;
-//}
-
-///* fwrite example : write buffer */
-//#include <stdio.h>
-
-//int main ()
-//{
-//    FILE * pFile;
-//    long long buffer[] = { 'x' , 'y' , 'z' };
-//    pFile = fopen ( "myfile.txt" , "wb" );
-//    fwrite (buffer , 130000, sizeof(buffer) , pFile );
-//    fclose (pFile);
-//    return 0;
-//}
-
 #include <fstream>
+#include <iostream>
+#include <QFile>
+#include <QDebug>
+
+using namespace std;
+#define SizeBlock ((5<<20) - 1)
 
 int main()
 {
-    std::ofstream ofs("ouput.img", std::ios::binary | std::ios::out);
-    ofs.seekp((1024<<20) - 1);
-    ofs.write("", 1);
+    ofstream ofs("output.raw", ios::out);
+    ofs.write("Hola", 4);
+    cout << SizeBlock << endl;
+    ofs.seekp(SizeBlock);
+    ofs.write("",1);
+    ofs.close();
+
+    cout << "Leyendo el archivo" << endl;
+    ifstream ifs("output.raw", ios::in);
+
+    char linea[128];
+    long contador = 0L;
+
+    if(ifs.fail())
+        cerr << "Error al abrir el ifs output.raw" << endl;
+    else
+        while(ifs.tellg() != SizeBlock)
+        {
+//            ifs.getline(linea, sizeof(linea));
+            ifs.seekg(contador, ios::beg);
+            ifs.read(linea, sizeof(linea));
+            cout << linea << endl;
+            contador += sizeof(linea);
+            if((contador % 24)==0)
+            {
+                cout << "CONTINUA...";
+                cin.get();
+            }
+        }
+    ifs.close();
+    return 0;
 }
