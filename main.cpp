8ff8b46f67e5b4af17d7e6ae81f041725fa122e4
daniel7 <darayavilla@hotmail.com>
2013-05-26 19:37:31 -0600

    varas en main.cpp

--- main.cpp
+++ main.cpp
@@ -11,105 +11,111 @@ using namespace std;
 
 int main()
 {
+
+
+// *****************************************************************************
+// LECTURA DEL ARBOL DE CARPETA Y ARCHIVO N-ARIO
+// *****************************************************************************
+
 // *****************************************************************************
 // PRUEBA PARA ESCRIBIR REGISTRO EN DISCO
 // *****************************************************************************
-    std::string name = "file1";
-    std::string data = "H";
-    unsigned short size = 3;
-    IRecordDataType *record1 = new RecordDataType<std::string>(name, data, size);
-    BTRecordFileMetadata *p = new BTRecordFileMetadata();
-//------------------------------------
-    std::string name2 = "file2";
-    std::string data2 = "R";
-    unsigned short size2 = 3;
-    IRecordDataType *record2 = new RecordDataType<std::string>(name2, data2, size2);
-//------------------------------------
-    std::string name3 = "file3";
-    std::string data3 = "D";
-    unsigned short size3 = 3;
-    IRecordDataType *record3 = new RecordDataType<std::string>(name3, data3, size3);
-// ------------------------------------
-    std::string name4 = "file4";
-    std::string data4 = "A";
-    unsigned short size4 = 3;
-    IRecordDataType *record4 = new RecordDataType<std::string>(name4, data4, size4);
+//    std::string name = "file1";
+//    std::string data = "H";
+//    unsigned short size = 3;
+//    IRecordDataType *record1 = new RecordDataType<std::string>(name, data, size);
+//    BTRecordFileMetadata *p = new BTRecordFileMetadata();
+////------------------------------------
+//    std::string name2 = "file2";
+//    std::string data2 = "R";
+//    unsigned short size2 = 3;
+//    IRecordDataType *record2 = new RecordDataType<std::string>(name2, data2, size2);
+////------------------------------------
+//    std::string name3 = "file3";
+//    std::string data3 = "D";
+//    unsigned short size3 = 3;
+//    IRecordDataType *record3 = new RecordDataType<std::string>(name3, data3, size3);
+//// ------------------------------------
+//    std::string name4 = "file4";
+//    std::string data4 = "A";
+//    unsigned short size4 = 3;
+//    IRecordDataType *record4 = new RecordDataType<std::string>(name4, data4, size4);
 
-    std::string name5 = "file5";
-    std::string data5 = "B";
-    unsigned short size5 = 3;
-    IRecordDataType *record5 = new RecordDataType<std::string>(name5, data5, size5);
+//    std::string name5 = "file5";
+//    std::string data5 = "B";
+//    unsigned short size5 = 3;
+//    IRecordDataType *record5 = new RecordDataType<std::string>(name5, data5, size5);
 
-    std::string name6 = "file6";
-    std::string data6 = "C";
-    unsigned short size6 = 3;
-    IRecordDataType *record6 = new RecordDataType<std::string>(name6, data6, size6);
+//    std::string name6 = "file6";
+//    std::string data6 = "C";
+//    unsigned short size6 = 3;
+//    IRecordDataType *record6 = new RecordDataType<std::string>(name6, data6, size6);
 
-    std::string name7 = "file7";
-    std::string data7 = "E";
-    unsigned short size7 = 3;
-    IRecordDataType *record7 = new RecordDataType<std::string>(name7, data7, size7);
+//    std::string name7 = "file7";
+//    std::string data7 = "E";
+//    unsigned short size7 = 3;
+//    IRecordDataType *record7 = new RecordDataType<std::string>(name7, data7, size7);
 
-    std::string name8 = "file8";
-    std::string data8 = "F";
-    unsigned short size8 = 3;
-    IRecordDataType *record8 = new RecordDataType<std::string>(name8, data8, size8);
+//    std::string name8 = "file8";
+//    std::string data8 = "F";
+//    unsigned short size8 = 3;
+//    IRecordDataType *record8 = new RecordDataType<std::string>(name8, data8, size8);
 
-    std::string name9 = "file3";
-    std::string data9 = "G";
-    unsigned short size9 = 3;
-    IRecordDataType *record9 = new RecordDataType<std::string>(name9, data9, size9);
+//    std::string name9 = "file3";
+//    std::string data9 = "G";
+//    unsigned short size9 = 3;
+//    IRecordDataType *record9 = new RecordDataType<std::string>(name9, data9, size9);
 
-    std::string name10 = "file3";
-    std::string data10 = "Z";
-    unsigned short size10 = 3;
-    IRecordDataType *record10 = new RecordDataType<std::string>(name10, data10, size10);
+//    std::string name10 = "file3";
+//    std::string data10 = "Z";
+//    unsigned short size10 = 3;
+//    IRecordDataType *record10 = new RecordDataType<std::string>(name10, data10, size10);
 
 
-    DLL<IRecordDataType * > *list1 = new DLL<IRecordDataType *>(), *list2 = new DLL<IRecordDataType *>(),
-            *list3 = new DLL<IRecordDataType *>(), *list4 = new DLL<IRecordDataType *>()
-            , *list5 = new DLL<IRecordDataType *>(), *list6 = new DLL<IRecordDataType *>()
-            , *list7 = new DLL<IRecordDataType *>(), *list8 = new DLL<IRecordDataType *>()
-            , *list9 = new DLL<IRecordDataType *>(), *list10 = new DLL<IRecordDataType *>();
-    list1->insertAtFront(record1);
-    list2->insertAtFront(record2);
-    list3->insertAtFront(record3);
-    list4->insertAtFront(record4);
-    list5->insertAtFront(record5);
-    list6->insertAtFront(record6);
-    list7->insertAtFront(record7);
-    list8->insertAtFront(record8);
-    list9->insertAtFront(record9);
-    list10->insertAtFront(record10);
-    //------------------------------------
+//    DLL<IRecordDataType * > *list1 = new DLL<IRecordDataType *>(), *list2 = new DLL<IRecordDataType *>(),
+//            *list3 = new DLL<IRecordDataType *>(), *list4 = new DLL<IRecordDataType *>()
+//            , *list5 = new DLL<IRecordDataType *>(), *list6 = new DLL<IRecordDataType *>()
+//            , *list7 = new DLL<IRecordDataType *>(), *list8 = new DLL<IRecordDataType *>()
+//            , *list9 = new DLL<IRecordDataType *>(), *list10 = new DLL<IRecordDataType *>();
+//    list1->insertAtFront(record1);
+//    list2->insertAtFront(record2);
+//    list3->insertAtFront(record3);
+//    list4->insertAtFront(record4);
+//    list5->insertAtFront(record5);
+//    list6->insertAtFront(record6);
+//    list7->insertAtFront(record7);
+//    list8->insertAtFront(record8);
+//    list9->insertAtFront(record9);
+//    list10->insertAtFront(record10);
+//    //------------------------------------
 
-    BTRecordFile *file = new BTRecordFile(p);
-    file->insertRecord2Disk(list1); // inserta datos en disco
-    file->insertRecord2Disk(list2);
-    file->insertRecord2Disk(list3);
-    file->insertRecord2Disk(list4);
-    file->insertRecord2Disk(list5);
-    file->insertRecord2Disk(list6);
-    file->insertRecord2Disk(list7);
-    file->insertRecord2Disk(list8);
-    file->insertRecord2Disk(list9);
-    file->readALLRecordsFromDisk();     // imprime los datos que están en disco
-// PRUEBAS DE BORRADO DE DATOS EN EL DISCO
-    file->deleteRecordFromDisk(3);  // borra datos en disco
-    file->readALLRecordsFromDisk();
-    file->deleteRecordFromDisk(5);
-    file->readALLRecordsFromDisk();
-    file->deleteRecordFromDisk(6);
-    file->readALLRecordsFromDisk();
-    file->deleteRecordFromDisk(2);
-    file->readALLRecordsFromDisk();
+//    BTRecordFile *file = new BTRecordFile(p);
+//    file->insertRecord2Disk(list1); // inserta datos en disco
+//    file->insertRecord2Disk(list2);
+//    file->insertRecord2Disk(list3);
+//    file->insertRecord2Disk(list4);
+//    file->insertRecord2Disk(list5);
+//    file->insertRecord2Disk(list6);
+//    file->insertRecord2Disk(list7);
+//    file->insertRecord2Disk(list8);
+//    file->insertRecord2Disk(list9);
+//    file->readALLRecordsFromDisk();     // imprime los datos que están en disco
+//// PRUEBAS DE BORRADO DE DATOS EN EL DISCO
+//    file->deleteRecordFromDisk(3);  // borra datos en disco
+//    file->readALLRecordsFromDisk();
+//    file->deleteRecordFromDisk(5);
+//    file->readALLRecordsFromDisk();
+//    file->deleteRecordFromDisk(6);
+//    file->readALLRecordsFromDisk();
+//    file->deleteRecordFromDisk(2);
+//    file->readALLRecordsFromDisk();
 
-    file->insertRecord2Disk(list3);
-    file->readALLRecordsFromDisk();
-    file->insertRecord2Disk(list10);
-    file->readALLRecordsFromDisk();
+//    file->insertRecord2Disk(list3);
+//    file->readALLRecordsFromDisk();
+//    file->insertRecord2Disk(list10);
+//    file->readALLRecordsFromDisk();
 
-    file->readOneRecordFromDisk(3);
+//    file->readOneRecordFromDisk(3);
 
 
 
