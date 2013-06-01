5fcc16e83bcfa6f898924a43133407b221275b35
Howar <howar@howar-HP-G42-Notebook-PC.(none)>
2013-05-31 18:29:08 -0600

    logs now

--- util/parser/Logs.cpp
+++ util/parser/Logs.cpp
@@ -3,3 +3,13 @@
 Logs::Logs()
 {
 }
+inline std::ofstream& logfile()
+{
+      static std::ofstream log("Logfile.log");
+      return log;
+}
+
+void Logs::logs(const char *location, const char *msg)
+{
+    logfile()<<"Run Proyect"<<location<<":"<<msg<<"\n";
+}
--- util/parser/Logs.h
+++ util/parser/Logs.h
@@ -1,10 +1,17 @@
 #ifndef LOGS_H
 #define LOGS_H
 
+#include <fstream>
+using namespace std;
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+#define AT __FILE__ ":" TOSTRING(__LINE__)
+
 class Logs
 {
 public:
     Logs();
+    void logs(const char *location, const char *msg);
 };
 
 #endif // LOGS_H
