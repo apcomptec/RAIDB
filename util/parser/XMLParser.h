134fce7de13701bf15c50067ebcb01d434b015dc
daniel7 <darayavilla@hotmail.com>
2013-06-21 14:41:03 -0600

    ACTUALIZACION

--- util/parser/XMLParser.h
+++ util/parser/XMLParser.h
@@ -22,8 +22,8 @@ using namespace std;
 class XMLParser: public IParser{
 
 private:
-    QString _pathXMLFile;       // Archivo XML a leer
-    QString _wpathXMLFile;      // Archivo XML a escribir
+    QString _pathXMLFile;       // Archivo XML a leer PRUEBA
+    QString _wpathXMLFile;      // Archivo XML a escribir PRUEBA
     QString _pathBACKUP;        // Archivo XML del Back Up
 //    QDomDocument _documento;
 //    QDomElement _xmlraiz;       // Crea un nodo root
@@ -35,14 +35,11 @@ public:
     ~XMLParser();       // Destructor
 
 // ----- MÉTODOS DE LA INTERFAZ IPARSER -----
-    //virtual void createFile();  // Crea un XML
     virtual void generateBackUp(unsigned short pAmountDisks,
                      unsigned short pAmountDiskGroups,
                      unsigned short pAmountUsers);  // Crea un XML
-    void writeFileData( QString pPath );
-    void saveFile();
-    virtual void readFile();    // Lee un XML
-    virtual void writeFile();   // Escribe un XML
+    virtual void readFile();    // Lee un XML PRUEBA
+    virtual void writeFile();   // Escribe un XML PRUEBA
 
 };
 
